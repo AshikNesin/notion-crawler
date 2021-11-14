baecf9e0d740b6512842eea9dceb17c9a9d36a12
Ashik Nesin <mail@ashiknesin.com>
2021-11-14 18:17:11 +0530

    Init Commit

---
+++ index.js
@@ -0,0 +1,104 @@
+import {
+  getAllPagesInSpace,
+  getCanonicalPageId,
+  uuidToId,
+  getBlockTitle,
+  getTextContent,
+  normalizeTitle,
+} from "notion-utils";
+import { NotionAPI } from "notion-client";
+const notion = new NotionAPI();
+
+export default async function notionCrawler(
+  rootNotionPageId,
+  spaceId,
+  config = {}
+) {
+  const pageMap = await getAllPagesInSpace(
+    rootNotionPageId,
+    spaceId,
+    notion.getPage.bind(notion),
+    {
+      traverseCollections: true,
+      uuid: true,
+      ...config,
+    }
+  );
+
+  const pageBlocks = Object.keys(pageMap).reduce((map, pageId) => {
+    const notionPageId = uuidToId(pageId);
+    const recordMap = pageMap[pageId];
+    if (!recordMap) {
+      throw new Error(`Error loading page "${pageId}"`);
+    }
+
+    const canonicalPageId =
+      rootNotionPageId === (pageId || "").split("-").join("")
+        ? "/"
+        : getCanonicalPageId(pageId, recordMap, {
+            uuid: false,
+          });
+    const block = pageMap[pageId].block[pageId];
+    const blockValue = block.value;
+    const title = getBlockTitle(blockValue, recordMap);
+
+    const response = {
+      ...map,
+      [notionPageId]: {
+        id: notionPageId,
+        parentId: blockValue.parent_id.split("-").join(""),
+        canonicalPageId,
+        title,
+        //   rawData: blockValue,
+      },
+    };
+
+    if (blockValue.parent_table === "collection") {
+      const collection = pageMap[pageId].collection[blockValue.parent_id];
+      const collectionName = getTextContent(collection.value.name);
+      const canonicalPageId = normalizeTitle(collectionName);
+      response[notionPageId].collection = {
+        name: collectionName,
+        canonicalPageId: canonicalPageId,
+        rawData: collection,
+      };
+      response[
+        notionPageId
+      ].canonicalPageId = `${canonicalPageId}/${response[notionPageId].canonicalPageId}`;
+    }
+
+    return response;
+  }, {});
+
+  const appendParentSlug = (pageId, previous = "") => {
+    const page = pageBlocks[pageId];
+    if (!page?.parentId || !pageBlocks[page.parentId]) {
+      return previous;
+    }
+    return appendParentSlug(
+      page.parentId,
+      `${pageBlocks[page.parentId].canonicalPageId}/${previous}`
+    );
+  };
+
+  const notionPageIdToSlugMapper = {};
+
+  for (const key in pageBlocks) {
+    pageBlocks[key].slug = `${appendParentSlug(
+      key,
+      pageBlocks[key].canonicalPageId
+    )}`;
+
+    // TODO: Temp fix. Refactor this.
+    if (pageBlocks[key].slug.startsWith("//")) {
+      pageBlocks[key].slug = pageBlocks[key].slug.slice(2);
+    }
+    notionPageIdToSlugMapper[key] = pageBlocks[key].slug;
+  }
+
+  return {
+    pageBlocks,
+    notionPageIdToSlugMapper,
+    pageMap,
+  };
+}
---
+++ readme.md
@@ -0,0 +1,48 @@
+# notion-crawler
+
+> Easily crawl your public notion pages
+
+## Install
+
+```sh
+npm install notion-crawler
+```
+
+## Usage
+
+```js
+import notionCrawler from "notion-crawler";
+
+const { pageBlocks, notionPageIdToSlugMapper, pageMap } = notionCrawler(
+  rootNotionPageId,
+  spaceId,
+  config
+);
+```
+
+## API
+
+### notionCrawler(rootNotionPageId, spaceId?, config?)
+
+#### rootNotionPageId
+
+Type: `string`
+
+You can find it in at the end of your notion public url
+
+For example, in case of https://ashiknesin.notion.site/AshikNesin-com-d95d7c8c5eaf40e1a7ae629f4aba0000
+
+`d95d7c8c5eaf40e1a7ae629f4aba0000` is rootNotionPageId.
+
+#### spaceId
+
+Type: `string`
+
+##### config
+
+Type: `object`
+Default: `{}`
+
+## Credits
+
+This module is hevily based on [nextjs-notion-starter-kit](https://github.com/transitive-bullshit/nextjs-notion-starter-kit) by [Travis Fischer](https://transitivebullsh.it)
